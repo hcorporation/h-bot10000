73522984e74eeb272d82002158668719e22e6d75
_r <122609604+neeeerrd@users.noreply.github.com>
2023-06-29 16:20:33 +0800

    allow two posts in one minute

--- index.py
+++ index.py
@@ -15,21 +15,20 @@ def run():
   reply_authors = []
   global submission
   print("letter h funny haha")
-  submissions = reddit.subreddit("theletterh").new(limit=1)
-  for submissionss in submissions:
-    submission = submissionss
-  for tlc in submission.comments:
-    for reply in tlc.replies:
-      reply_authors.append(reply.author)
-    if tlc.author == "h-bot9000" and "h-bot10000" not in reply_authors:
-      print("https://reddit.com/r/theletterh/comments/"+str(submission)+"/comment/"+str(tlc))    
-      tlc.reply("h")
-      reply_authors = []
-      print("sent without error")
-    elif "h-bot10000" in reply_authors:
-      print("https://reddit.com/r/theletterh/comments/"+str(submission)+"/comment/"+str(tlc))
-      reply_authors = []
-      print("already sent reply")
+  submissions = reddit.subreddit("theletterh").new(limit=2)
+  for submission in submissions:
+    for tlc in submission.comments:
+      for reply in tlc.replies:
+        reply_authors.append(reply.author)
+      if tlc.author == "h-bot9000" and "h-bot10000" not in reply_authors:
+        print("https://reddit.com/r/theletterh/comments/"+str(submission)+"/comment/"+str(tlc))    
+        tlc.reply("h")
+        reply_authors = []
+        print("sent without error")
+      elif "h-bot10000" in reply_authors:
+        print("https://reddit.com/r/theletterh/comments/"+str(submission)+"/comment/"+str(tlc))
+        reply_authors = []
+        print("already sent reply")
   print("waiting...")
   time.sleep(60)
   print("IM BACKKKKKKK")
