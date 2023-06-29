c733735f83f74bd4cac3e9d0bf5d058ae8e3ab00
_r <122609604+neeeerrd@users.noreply.github.com>
2023-06-29 16:06:56 +0800

    ratelimits r gone

--- index.py
+++ index.py
@@ -13,23 +13,26 @@ reddit = praw.Reddit(
 )
 def run():
   reply_authors = []
-  try:
-    for submission in reddit.subreddit("theletterh").stream.submissions():
-      for tlc in submission.comments:
-        for reply in tlc.replies:
-          reply_authors.append(reply.author)
-        if tlc.author == "h-bot9000" and "h-bot10000" not in reply_authors:
-          print("https://reddit.com/r/theletterh/comments/"+str(submission)+"/comment/"+str(tlc))    
-          tlc.reply("h")
-          reply_authors = []
-          print("sent without error")
-        elif "h-bot10000" in reply_authors:
-          print("https://reddit.com/r/theletterh/comments/"+str(submission)+"/comment/"+str(tlc))
-          reply_authors = []
-          print("already sent reply")
-  except:
-    print("rate limited D: waiting one hour")
-    time.sleep(3600)
-    run()
+  global submission
+  print("letter h funny haha")
+  submissions = reddit.subreddit("theletterh").new(limit=1)
+  for submissionss in submissions:
+    submission = submissionss
+  for tlc in submission.comments:
+    for reply in tlc.replies:
+      reply_authors.append(reply.author)
+    if tlc.author == "h-bot9000" and "h-bot10000" not in reply_authors:
+      print("https://reddit.com/r/theletterh/comments/"+str(submission)+"/comment/"+str(tlc))    
+      tlc.reply("h")
+      reply_authors = []
+      print("sent without error")
+    elif "h-bot10000" in reply_authors:
+      print("https://reddit.com/r/theletterh/comments/"+str(submission)+"/comment/"+str(tlc))
+      reply_authors = []
+      print("already sent reply")
+  print("waiting...")
+  time.sleep(60)
+  print("IM BACKKKKKKK")
+  run()
 
 run()
