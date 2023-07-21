21095ffb98ef7a5019c3f4ece75b40501b8ce369
neeeerrd <emkelleher@protonmail.com>
2023-07-21 11:20:36 +0100

    bug fix just in time

--- helper/usefulcomponents.py
+++ helper/usefulcomponents.py
@@ -9,15 +9,6 @@ class Components:
     def new_submissions(self):
         submissions = self.reddit.subreddit("theletterh").stream.submissions(skip_existing=True)
         return submissions
-    def check_if_post_is_valid(self, submission):
-        repliers = []
-        for tlc in submission.comments:
-            for reply in tlc.replies:
-                repliers.append(reply.author)
-            if tlc.author == "h-bot9000" and "h-bot10000" not in repliers:
-                return True
-            elif tlc.author != "h-bot9000" or "h-bot10000" in repliers:
-                return False
     def new_mentions(self):
         mentions = self.reddit.inbox.mentions
         return praw.models.util.stream_generator(mentions, skip_existing=True)
--- src/defaultreplier.py
+++ src/defaultreplier.py
@@ -6,20 +6,22 @@ def defaultreplier(reddit):
   components = Components(reddit)
   submissions = components.new_submissions()
   for submission in submissions:
-    valid = components.check_if_post_is_valid(submission=submission)
+    repliers = []
     for tlc in submission.comments:
-      if valid == True:       
-        prob = random.randint(0,100000)
-        prob2 = random.randint(0,10000000000)
-        capitalh = random.randint(0,1)
-        if prob == 1 & prob2 != 1:
-          tlc.reply("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
-        elif prob == 1 & prob2 == 1:
-          tlc.reply("e (tHere is a one in like a bajillion cHance that i reply witH e. if you see tHis, consider it qood luck)")
-        elif capitalh == 1:
-          tlc.reply("H")
-        else:
-          tlc.reply("h")
-      else:
-        return
+        for reply in tlc.replies:
+            repliers.append(reply.author)
+            if tlc.author == "h-bot9000" and "h-bot10000" not in repliers:
+              prob = random.randint(0,100000)
+              prob2 = random.randint(0,10000000000)
+              capitalh = random.randint(0,1)
+              if prob == 1 & prob2 != 1:
+                tlc.reply("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
+              elif prob == 1 & prob2 == 1:
+                tlc.reply("e (tHere is a one in like a bajillion cHance that i reply witH e. if you see tHis, consider it qood luck)")
+              elif capitalh == 1:
+                tlc.reply("H")
+              else:
+                tlc.reply("h")
+            else:
+              return
         
