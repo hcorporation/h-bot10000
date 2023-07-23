40f8074b705ab15b681c48f84e7bd126a92068b9
neeeerrd <emkelleher@protonmail.com>
2023-07-23 12:46:25 +0100

    fixes #1

--- src/forbiddenletter.py
+++ src/forbiddenletter.py
@@ -6,7 +6,7 @@ def forbiddenletter(reddit):
         if username != "":
             for comment in reddit.redditor(username).comments.new(limit=None):
                 forbiddencount = forbiddencount + comment.body.count("g")
-            mention.reply(f'u/{username} has said the forbidden letter **{forbiddencount}** times.\n\n*****\n\n^(psst, if theres a problem, report it) [^here](https://github.com/neeeerrd/h-bot10000/issues)')
+            mention.reply(f'u/{username} has said the forbidden letter **{forbiddencount}** times.\n\n*****\n\n^(psst, if theres a problem, report it) [^(here)](https://github.com/neeeerrd/h-bot10000/issues)')
         else:
             mention.reply("you need a username to check.")
         mention.mark_read()
@@ -14,27 +14,15 @@ def forbiddenletter(reddit):
     components = Components(reddit)
     new_mentions = components.new_mentions()
     for mention in new_mentions:
-        if mention.new:
-            if "u/h-bot10000 !forbiddenLetter u/" in mention.body:
-                forbiddencount = 0
-                username = mention.body.replace("u/h-bot10000 !forbiddenLetter u/","")
-                if "/" in username:
-                    trueusername = username.replace("/","")
-                try:
-                    replier(mention, forbiddencount, trueusername)
-                except:
-                    mention.reply("an error occurred. doesn't seem right? report it [here](https://github.com/neeeerrd/h-bot10000/issues)")
-                    mention.mark_read()
-            elif "u/h-bot10000 !forbiddenLetter /u/" in mention.body:
-                forbiddencount = 0
-                username = mention.body.replace("u/h-bot10000 !forbiddenLetter /u/","")
-                if "/" in username:
-                    trueusername = username.replace("/","")
-                try:
-                    replier(mention,forbiddencount,trueusername)
-                except:
-                    mention.reply("an error occurred. doesn't seem right? report it [here](https://github.com/neeeerrd/h-bot10000/issues)")
-                    mention.mark_read()
-            else:
-                mention.reply("h")
-                mention.mark_read()
+        if "u/h-bot10000 !forbiddenLetter" in mention.body:
+            forbiddencount = 0
+            if "/u/h-bot10000" in mention.body:
+                username = mention.body.replace("/u/h-bot10000 !forbiddenLetter ", "")
+            elif "u/h-bot10000" in mention.body:
+                username = mention.body.replace("u/h-bot10000 !forbiddenLetter ", "")
+            try:
+                replier(mention, forbiddencount, username)
+            except:
+                mention.reply("an error occurred. doesn't seem right? report it [here](https://github.com/neeeerrd/h-bot10000/issues)")
+        elif "among us" in mention.body:
+            mention.reply("h")
