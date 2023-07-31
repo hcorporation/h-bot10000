60a30263ee95eeda56fac78c6c51eabd52acf725
neeeerrd <emkelleher@protonmail.com>
2023-07-31 10:22:38 +0100

    add features suqquested by u/cryxtal and u/Actual_Tumbleweed814

--- src/defaultreplier.py
+++ src/defaultreplier.py
@@ -8,6 +8,8 @@ def defaultreplier(reddit):
     print("new comment")   
     prob = random.randint(0,100000)
     prob2 = random.randint(0,10000000000)
+    prob3 = random.randint(0,8000)
+    prob4 = random.randint(0,25)
     capitalh = random.randint(0,1)
     if prob == 1 & prob2 != 1:
       comment.reply("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
@@ -15,6 +17,10 @@ def defaultreplier(reddit):
       comment.reply("e (tHere is a one in like a bajillion cHance that i reply witH e. if you see tHis, consider it qood luck)")
     elif capitalh == 1:
       comment.reply("H")
+    elif prob3 == 1:
+      comment.reply("pc")
+    elif prob4 == 1:
+      comment.reply("h is cool")
     else:
       comment.reply("h")
     print("replied")        
