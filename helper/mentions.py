93ca25ffc4b15e5797c3020566369404c890e251
_r <122609604+neeeerrd@users.noreply.github.com>
2023-12-29 17:57:47 +0000

    clean up code
    removed ai component, cleaned up some useless/confusing code

--- helper/mentions.py
+++ helper/mentions.py
@@ -1,12 +1,11 @@
 from helper.usefulcomponents import Components
-from src.aireplier import ai
 from src.forbiddenletter import forbiddenletter
 from src.topposts import topposts
 
 def mentionuser(reddit, key):
     components = Components(reddit)
     new_mentions = components.new_mentions()
-    error_reply = "an error occurred. doesn't seem right? report it [here](https://github.com/neeeerrd/h-bot10000)"
+    error_reply = "an error occurred. doesn't seem right? report it [here](https://github.com/hcorporation/h-bot10000)"
     for mention in new_mentions:
         if "!forbiddenLetter" in mention.body:
             forbiddencount = 0
@@ -14,13 +13,6 @@ def mentionuser(reddit, key):
             username = username.replace("/u/", "")
             try:
                 reply = forbiddenletter(reddit, username)
-                mention.reply(reply)
-            except:
-                mention.reply(error_reply)
-        elif "!betaAi" in mention.body:    
-            prompt = mention.body.replace("/u/h-bot10000 !betaAi ", "")
-            try:
-                reply = ai(prompt, key, mention.author)
                 mention.reply(reply)
             except:
                 mention.reply(error_reply)
--- helper/oauthvalidater.py
+++ helper/oauthvalidater.py
@@ -1,6 +1,6 @@
 from helper.reddit import reddit
 def validater():
-    if reddit.read_only != True:
+    if reddit.read_only == False:
         print("successfully connected to reddit.")
     else:
         print("could not connect to reddit. have you provided the correct credentials?")
